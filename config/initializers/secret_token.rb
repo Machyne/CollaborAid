85bca2b3c2d42f7497f2e5a962103d13476773cb
Machyne <mcotter222@gmail.com>
2014-03-09 18:13:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collaboraid::Application.config.secret_key_base = 'e0ce1b6df8e4b35cefaff9079dfa7292623b8692fcd9bf5eca65d43a679e673e6c21dbe8cb570216fb222d398d718393a4ee54182d4b333fa87dc3092e88cfd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Collaboraid::Application.config.session_store :cookie_store, key: '_collaboraid_session'
