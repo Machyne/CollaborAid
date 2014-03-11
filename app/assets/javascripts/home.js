0a3f9fb39e48bf8e7f39a123c8e6aa20efde6699
Machyne <mcotter222@gmail.com>
2014-03-10 23:47:28 -0500

    add a dummy map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  var map = L.mapbox.map('map', MAP_URL).setView([34.7, 38.0], 6);
+  // Pass features and a custom factory function to the map
+  map.featureLayer.setGeoJSON(geoJson);
+});
